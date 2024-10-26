9f31efe15bd33df87fa3c77a7da3fe8021a3c64d
Jose Perdomo <jose.gabriel92@hotmail.com>
2024-10-26 15:03:41 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim () !== "") {
+        const taskItem = document.createElement("li");
+        taskItem.textContent = taskInput.value;
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+    } else {
+        alert("PLEASE ENTER A TASK!");
+    }
+}
